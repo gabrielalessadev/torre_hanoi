09b3cc3f5c9d49de489b50b43e493db3e5daf658
Gab <gabrielaledev@gmail.com>
2025-06-09 16:08:18 -0300

    Implementada a função de reiniciar o jogo quando estiver jogando e feito mais alguns testes jogando e verificando historico

--- main.c
+++ main.c
@@ -7,21 +7,23 @@
 // gcc -o hanoi_game main.c hanoi.c pilha.c historico.c -Wall -Wextra
 // .\hanoi_game.exe
 
-void rodar_partida(RegistroPartida** inicio_historico, int num_discos) {
+void rodar_partida(RegistroPartida** inicio_historico, int num_discos, const char* nome_jogador) {
     JogoHanoi jogo;
-    char nome_jogador[TAMANHO_MAX_NOME];
-
-    printf("Digite seu nome: ");
-    fgets(nome_jogador, sizeof(nome_jogador), stdin);
-    nome_jogador[strcspn(nome_jogador, "\n")] = 0;
-
     iniciar_jogo(&jogo, num_discos);
 
     while (!verificar_vitoria(&jogo)) {
         exibir_jogo(&jogo);
         int origem, destino;
-        printf("Mover do pino (0,1,2) para o pino (0,1,2): ");
-        scanf("%d %d", &origem, &destino);
+        printf("Mover do pino (0,1,2) para (0,1,2) (ou -1 para reiniciar): ");
+        scanf("%d", &origem);
+
+        if (origem == -1) {
+            printf("\n Reiniciando a partida! \n");
+            iniciar_jogo(&jogo, num_discos);
+            continue; 
+        }
+
+        scanf("%d", &destino);
         while(getchar() != '\n');
 
         if (!fazer_movimento(&jogo, origem, destino)) {
@@ -29,9 +31,10 @@ void rodar_partida(RegistroPartida** inicio_historico, int num_discos) {
             getchar();
         }
     }
+
     exibir_jogo(&jogo);
     printf("\n*** Parabens, %s, voce venceu em %d movimentos! ***\n", nome_jogador, jogo.movimentos);
-    adicionar_registro(inicio_historico, jogo.movimentos, nome_jogador, jogo.num_discos);
+    adicionar_registro(inicio_historico, jogo.movimentos, nome_jogador, num_discos);
     salvar_historico_em_arquivo(*inicio_historico);
 }
 
@@ -41,7 +44,7 @@ int main() {
 
     int escolha;
     do {
-        printf("\n Menu Torre de Hanoi\n");
+        printf("\n Menu Torre de Hanoi \n");
         printf("1. Novo Jogo\n");
         printf("2. Ver Historico\n");
         printf("3. Buscar no Historico\n");
@@ -53,10 +56,23 @@ int main() {
         switch(escolha) {
             case 1: {
                 int num_discos;
+                char nome_jogador[TAMANHO_MAX_NOME];
+                char jogar_novamente;
+
+                printf("Digite seu nome: ");
+                fgets(nome_jogador, sizeof(nome_jogador), stdin);
+                nome_jogador[strcspn(nome_jogador, "\n")] = 0;
+
                 printf("Digite o numero de discos: ");
                 scanf("%d", &num_discos);
                 while(getchar() != '\n');
-                rodar_partida(&inicio_historico, num_discos);
+
+                do {
+                    rodar_partida(&inicio_historico, num_discos, nome_jogador);
+                    printf("\nJogar novamente com %d discos? (s/n): ", num_discos);
+                    scanf(" %c", &jogar_novamente);
+                    while(getchar() != '\n');
+                } while (tolower(jogar_novamente) == 's');
                 break;
             }
             case 2:
